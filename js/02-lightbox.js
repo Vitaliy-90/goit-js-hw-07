be154356883ff9240d7667401a1676f06e927dcc
Vitaliy-90 <savavitalij1@gmail.com>
2023-06-18 20:09:45 +0300

    2131

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function makeImgItem(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+       alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+galleryList.insertAdjacentHTML("beforeend", makeImgItem(galleryItems));
+
+// Ініціалізуємо плагін лайтбокс та додаємо параметри
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionDelay: 250,
+});
